ecac2006a6c3a11f79dedb97246b0141561812f6
junxiang <jimmy871117@gmail.com>
2026-04-22 09:12:28 +0800

    feat: add zh-TW

--- src/locale/utils.ts
+++ src/locale/utils.ts
@@ -1,10 +1,16 @@
-export type LocaleName = "zh" | "en";
+export type LocaleName = "zh-TW" | "zh" | "en";
 
 export const locales = [
     {
+        name: "zh-TW",
+        fetcher: () => import("./lang/zh-TW.json"),
+        matcher: (_l: string) => _l.includes("zh-TW") || _l.includes("zh-HK") || _l.includes("zh-Hant"),
+        label: "中文-繁體",
+    },
+    {
         name: "zh",
         fetcher: () => import("./lang/zh.json"),
-        matcher: (_l: string) => _l.includes("zh-CN"),
+        matcher: (_l: string) => _l.includes("zh-CN") || _l.includes("zh-SG"),
         label: "中文-简体",
     },
     {
@@ -19,5 +25,5 @@ export const getBrowserLang = (): LocaleName => {
     const browserLang: string =
         navigator.language || (navigator as any).browserLanguage;
     const locale = locales.find((l) => l.matcher(browserLang));
-    return (locale?.name ?? locales[0].name) as LocaleName;
+    return (locale?.name ?? "zh-TW") as LocaleName;
 };
